c6a6621a851c3a1a3fd8ab2ffc3e326355e7ac54
daruma0309 <ojikan39@gmail.com>
2021-08-23 23:54:21 +0900

    use UVCcamera

--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,34 @@
 
 using namespace std;
 
-int main(void) {   
-  // 動画ファイルのパスの文字列を格納するオブジェクトを宣言する
-  std::string filepath = "video.h264";
+int main(int argc, char **argv) {
+  int camera;
+  if (argc != 2) {
+    cout << "[usage] ./play_video [camera_number]" << endl;
+    return 0;
+  } else {
+     camera = atoi(argv[1]);
+  }
   // 動画ファイルを取り込むためのオブジェクトを宣言する
-  cv::VideoCapture video;
+  cv::VideoCapture cap;
 
-  // 動画ファイルを開く
-  video.open(filepath);
-  if (video.isOpened() == false) {  // 動画ファイルが開けなかったときは終了する
+  // カメラを開く
+  cap.open(camera);
+  if (cap.isOpened() == false) {  // 動画ファイルが開けなかったときは終了する
     return 0;
   }
 
-  int    width, height, fourcc;
-  double fps;
-  width  = (int)video.get(cv::CAP_PROP_FRAME_WIDTH);	// フレーム横幅を取得
-  height = (int)video.get(cv::CAP_PROP_FRAME_HEIGHT);	// フレーム縦幅を取得
-  fps    = video.get(cv::CAP_PROP_FPS);			// フレームレートを取得
-
-  cout << "width, height, fps = " << width << ", " << height << ", " << fps << endl;
-
-  cv::Mat image ;// 画像を格納するオブジェクトを宣言する
-  while (1) {
-    video >> image;
-
-    if (image.empty() == true) break;
-
+  cv::Mat frame ;
+  while(cap.read(frame)) {
     // リサイズして描画
     cv::Mat small;
-    cv::resize(image, small, cv::Size(), 0.5, 0.5);
+    cv::resize(frame, small, cv::Size(), 0.8, 0.8);
     cv::imshow("showing", small); // ウィンドウに動画を表示する
 
-    if (cv::waitKey(0) == 'q') break; //qを押すと終了
+    if (cv::waitKey(1) == 'q') break; //qを押すと終了
   }
+  
+  cv::destroyAllWindows();
   return 0;
 }
+
